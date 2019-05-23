5762ccf41b1d213f7988955013c4276b83b4397d
Nobot_ <31627752+Yatheon@users.noreply.github.com>
2019-05-23 22:36:11 +0200

    s

--- src/SCTPSingleServer.java
+++ src/SCTPSingleServer.java
@@ -30,53 +30,57 @@ public class SCTPSingleServer implements Runnable {
         SctpServerChannel ssc = SctpServerChannel.open();
         InetSocketAddress serverAddr = new InetSocketAddress(SERVER_PORT);
         ssc.bind(serverAddr);
+
+        double packetSizeDouble = 10240;
+        int packetSize = 10240;
+        long fileSize = myFile.length();
+        int bytesLeft;
+
+
+        long fileLength = myFile.length();
+        float packetsToSend = (float) Math.ceil(fileLength / packetSizeDouble);
+        bytesLeft = (int)fileSize;
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(myFile));
+
         while (true) {
             //File myFile = new File("FileToSend.tar.gz");
-
-            double packetSizeDouble = 10240;
-            int packetSize = 10240;
-            long fileSize = myFile.length();
-            int bytesLeft;
-
-
-            long fileLength = myFile.length();
-            float packetsToSend = (float) Math.ceil(fileLength / packetSizeDouble);
             bytesLeft = (int)fileSize;
-            BufferedInputStream bis = new BufferedInputStream(new FileInputStream(myFile));
-
-            SctpChannel sc = ssc.accept();
-            sc.setOption(SO_SNDBUF, 212992);
-            for(int i = 0; i< packetsToSend; i++){
-                byte[] byteArray;
-                if(bytesLeft<packetSize) {
-                    byteArray = new byte[bytesLeft];
+            try {
 
 
+                SctpChannel sc = ssc.accept();
+                for (int i = 0; i < packetsToSend; i++) {
+                    byte[] byteArray;
+                    if (bytesLeft < packetSize) {
+                        byteArray = new byte[bytesLeft];
+
+
+                    } else {
+                        byteArray = new byte[packetSize];
+
+                    }
+                    bis.read(byteArray, 0, byteArray.length);
+                    ByteBuffer buf = ByteBuffer.wrap(byteArray);
+                    //System.out.println("Sending Packet : "+ (i+1));
+                    if (bytesLeft < packetSize) {
+                        //     System.out.println("Packet size: " + bytesLeft);
+                        bytesLeft -= bytesLeft;
+                    } else {
+                        //   System.out.println("Packet size: " + packetSize);
+                        bytesLeft -= packetSize;
+                    }
+                    //  System.out.println("Bytes left to send: "+ bytesLeft +"\n");
+                    MessageInfo messageInfo = MessageInfo.createOutgoing(null, 0);
+                    sc.send(buf, messageInfo);
+
+
+                    buf.clear();
                 }
-                else {
-                    byteArray = new byte[packetSize];
 
-                }
-                bis.read(byteArray, 0, byteArray.length);
-                ByteBuffer buf = ByteBuffer.wrap(byteArray);
-                //System.out.println("Sending Packet : "+ (i+1));
-                if(bytesLeft<packetSize) {
-               //     System.out.println("Packet size: " + bytesLeft);
-                    bytesLeft -= bytesLeft;
-                }
-                else {
-                 //   System.out.println("Packet size: " + packetSize);
-                    bytesLeft -= packetSize;
-                }
-              //  System.out.println("Bytes left to send: "+ bytesLeft +"\n");
-                MessageInfo messageInfo = MessageInfo.createOutgoing(null, 0);
-                sc.send(buf, messageInfo);
-
-
-                buf.clear();
+                sc.close();
+            }catch (Exception e){
+                System.out.println("Connection closed");
             }
-
-            sc.close();
         }
     }
 
